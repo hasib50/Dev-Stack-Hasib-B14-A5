f11ce60ceee0997fb6e5fc2888007b09014a34f5
hasib50 <alhasib2345@gmail.com>
2026-09-12 13:42:32 +0600

    Folder structure, tailwind, dasiUi Setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className='text-5xl text-red-500'>Dav STack</h2>
+      <button className="btn btn-outline btn-error">Error</button>
+
+    </>
+  )
+}
+
+export default App
